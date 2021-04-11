757faab847a56a5abcf29514c5baedc7ef1b6187
Raoni Madeira <raonimadeira@gboex.com.br>
2021-04-10 21:44:00 -0300

    desenv-initial

---
+++ src/components/ToDoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/ToDoList.css'
+
+const TodoList = () => {
+    return (
+        <div className="todo-cointainer">
+            <ul className="todo-list"></ul>
+        </div>
+    );
+};
+
+export default TodoList;
